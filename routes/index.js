dcc8200d05d14d50eff458d4d0c2cc726ef53c60
sachinraste <31955258+sachinraste@users.noreply.github.com>
2018-09-25 11:50:12 +0530

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,5 @@
-
+exports.index = function (letters) {
+	return function(req, res){
+	res.render('index', { title: 'Remote System Explorer',letters:letters,dataDrives:JSON.stringify(letters)});
+};
+};
